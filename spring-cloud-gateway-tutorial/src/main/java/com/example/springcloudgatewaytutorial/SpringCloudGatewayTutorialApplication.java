a72a6f2cc2bb0a62eb067e8d6e78f42aa709cba2
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-02-10 10:02:22 +0900

    first commit

---
+++ spring-cloud-gateway-tutorial/src/main/java/com/example/springcloudgatewaytutorial/SpringCloudGatewayTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewaytutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayTutorialApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-gateway-tutorial/src/test/java/com/example/springcloudgatewaytutorial/SpringCloudGatewayTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewaytutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
